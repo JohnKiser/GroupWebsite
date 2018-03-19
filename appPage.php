a21e2ae37e4a222103d0e0e547cfdd95e1d2c226
John Kiser <john.j.kiser@gmail.com>
2018-03-18 21:03:34 -0600

    Fixes

--- appPage.php
+++ appPage.php
@@ -14,6 +14,7 @@
 </style>
 	
 <body>
+	<?php require_once ('Navbar.php'); ?>
 	<h1 align="center">Group App -- Boss Battle Trading Card Game</h1>
 	<h4 align="center">Developed in Unity for Android</h4>
 	<br>
@@ -31,14 +32,14 @@
 			<div class="col-xl-8 ruledes">  <!-- rules & example cards column -->
 				<h4 align="center"><i>Rules & Regulations</i></h4><br>
 				<p>The rules for Boss Battle are still work in progress, but put most simply each player has a consummable amount of mana they can use to cast various monsters and spells.  Only monsters in the 'Boss Zone' can use the effects printed on their card, all other monsters can only attack or defend.  When the player takes a certain amount of damage, they lose the game!</p>
-				<div class-"imageboi">
-					<p align="center"><a href=""><img src="images/wambam_the_trashcan_man.png" width="20%" height="20%"></a>
-						<a href=""><img src="images/gently_used_pizza.png" width="20%" height="20%"></a></p> </div>
+				<div class="imageboi">
+					<p align="center"><a href=""><img src="images/wambam_the_trashcan_man.png"  alt="Garbage Boi" width="20%" height="20%"></a>
+						<a href=""><img src="images/gently_used_pizza.png" alt="DiscountPizza" width="20%" height="20%"></a></p> </div>
 			</div>
 			<div class="col-xl-2">  <!-- link to finished game column -->
 				<div class="linkboi">
 				<p align="center">Link to download <a href="">here</a></p>
-				<p align="center"><a href=""><img src="images/Almonzo Time Magazine.png" width="40%" height="60%"></a></p>
+				<p align="center"><a href=""><img src="images/Almonzo Time Magazine.png" alt="Almemezo" width="40%" height="60%"></a></p>
 		</div>
 			</div>
 		</div>
